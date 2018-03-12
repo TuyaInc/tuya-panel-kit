43b233d11156d9e35d38f536b17a00d6d06927cd
fri3nds <chenglong@tuya.com>
2018-03-12 15:47:19 +0800

    add I18N to export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import TYNativeApi from './api';
-import Strings from './i18n';
+import Strings from './i18n/strings';
+import I18N from './i18n';
 import TYNativeModule from './nativeModule';
 
 export {
   TYNativeApi,
   Strings,
+  I18N,
   TYNativeModule,
 };
