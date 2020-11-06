33373be2539a9fd569bb5b1b18003aa7af2b5e9e
tuyafe <tuyafe@tuya.com>
2020-11-06 15:41:58 +0800

    add sync.sh

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git config --global user.name "tuyafe"
+git config --global user.email "tuyafe@tuya.com"
+git fetch origin
+git checkout master
+git checkout TYNativeApi
+git branch
+git checkout master -- src/TYNativeApi.js
+git remote add github https://github.com/TuyaInc/tuya-panel-kit.git
+git push github -f
+git remote rm github
